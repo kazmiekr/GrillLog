545c82ca97cb49d082659f48338202f158a0de5c
kazmiekr <kazmiekr@gmail.com>
2015-09-28 17:46:55 -0400

    Adds the rest of the iOS project, excluding the parse keys

---
+++ iOS/GrillLog/AppDelegate.swift
@@ -0,0 +1,135 @@
+//
+//  AppDelegate.swift
+//
+//  Copyright 2011-present Parse Inc. All rights reserved.
+//
+
+import UIKit
+
+import Bolts
+import Parse
+
+// If you want to use any of the UI components, uncomment this line
+// import ParseUI
+
+// If you want to use Crash Reporting - uncomment this line
+// import ParseCrashReporting
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    //--------------------------------------
+    // MARK: - UIApplicationDelegate
+    //--------------------------------------
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Enable storing and querying data from Local Datastore. 
+        // Remove this line if you don't want to use Local Datastore features or want to use cachePolicy.
+        Parse.enableLocalDatastore()
+
+        // ****************************************************************************
+        // Uncomment this line if you want to enable Crash Reporting
+        // ParseCrashReporting.enable()
+        //
+        // Uncomment and fill in with your Parse credentials:
+        Parse.setApplicationId(PARSE_APPLICATION_ID, clientKey: PARSE_CLIENT_KEY)
+        //
+        // If you are using Facebook, uncomment and add your FacebookAppID to your bundle's plist as
+        // described here: https://developers.facebook.com/docs/getting-started/facebook-sdk-for-ios/
+        // Uncomment the line inside ParseStartProject-Bridging-Header and the following line here:
+        // PFFacebookUtils.initializeFacebook()
+        // ****************************************************************************
+
+        PFUser.enableAutomaticUser()
+
+        let defaultACL = PFACL();
+
+        // If you would like all objects to be private by default, remove this line.
+        defaultACL.setPublicReadAccess(true)
+
+        PFACL.setDefaultACL(defaultACL, withAccessForCurrentUser:true)
+
+        if application.applicationState != UIApplicationState.Background {
+            // Track an app open here if we launch with a push, unless
+            // "content_available" was used to trigger a background push (introduced in iOS 7).
+            // In that case, we skip tracking here to avoid double counting the app-open.
+
+            let preBackgroundPush = !application.respondsToSelector("backgroundRefreshStatus")
+            let oldPushHandlerOnly = !self.respondsToSelector("application:didReceiveRemoteNotification:fetchCompletionHandler:")
+            var noPushPayload = false;
+            if let options = launchOptions {
+                noPushPayload = options[UIApplicationLaunchOptionsRemoteNotificationKey] != nil;
+            }
+            if (preBackgroundPush || oldPushHandlerOnly || noPushPayload) {
+                PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+            }
+        }
+        if application.respondsToSelector("registerUserNotificationSettings:") {
+            let userNotificationTypes: UIUserNotificationType = [UIUserNotificationType.Alert, UIUserNotificationType.Badge, UIUserNotificationType.Sound]
+            let settings = UIUserNotificationSettings(forTypes: userNotificationTypes, categories: nil)
+            application.registerUserNotificationSettings(settings)
+            application.registerForRemoteNotifications()
+        } else {
+            let types: UIRemoteNotificationType = [UIRemoteNotificationType.Badge, UIRemoteNotificationType.Alert, UIRemoteNotificationType.Sound]
+            application.registerForRemoteNotificationTypes(types)
+        }
+
+        return true
+    }
+
+    //--------------------------------------
+    // MARK: Push Notifications
+    //--------------------------------------
+
+    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+        let installation = PFInstallation.currentInstallation()
+        installation.setDeviceTokenFromData(deviceToken)
+		installation.channels = ["global"]
+        installation.saveInBackground()
+        
+        /*PFPush.subscribeToChannelInBackground("global") { (succeeded: Bool, error: NSError?) in
+            if succeeded {
+                println("ParseStarterProject successfully subscribed to push notifications on the broadcast channel.");
+            } else {
+                println("ParseStarterProject failed to subscribe to push notifications on the broadcast channel with error = %@.", error)
+            }
+        }*/
+    }
+
+    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+        if error.code == 3010 {
+            print("Push notifications are not supported in the iOS Simulator.")
+        } else {
+            print("application:didFailToRegisterForRemoteNotificationsWithError: %@", error)
+        }
+    }
+
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+        PFPush.handlePush(userInfo)
+        if application.applicationState == UIApplicationState.Inactive {
+            PFAnalytics.trackAppOpenedWithRemoteNotificationPayload(userInfo)
+        }
+    }
+
+    ///////////////////////////////////////////////////////////
+    // Uncomment this method if you want to use Push Notifications with Background App Refresh
+    ///////////////////////////////////////////////////////////
+    // func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject], fetchCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
+    //     if application.applicationState == UIApplicationState.Inactive {
+    //         PFAnalytics.trackAppOpenedWithRemoteNotificationPayload(userInfo)
+    //     }
+    // }
+
+    //--------------------------------------
+    // MARK: Facebook SDK Integration
+    //--------------------------------------
+
+    ///////////////////////////////////////////////////////////
+    // Uncomment this method if you are using Facebook
+    ///////////////////////////////////////////////////////////
+    // func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject?) -> Bool {
+    //     return FBAppCall.handleOpenURL(url, sourceApplication:sourceApplication, session:PFFacebookUtils.session())
+    // }
+}
---
+++ iOS/GrillLog/SampleParseCredentials.swift
@@ -0,0 +1,11 @@
+//
+//  SampleParseCredentials.swift
+//  GrillLog
+//
+//  Created by Kevin Kazmierczak on 9/28/15.
+//  Copyright © 2015 Parse. All rights reserved.
+//
+
+// Replace your credentials here, uncomment them and copy as as ParseCredentials
+// let PARSE_APPLICATION_ID = "ABC123"
+// let PARSE_CLIENT_KEY = "ABC123"
